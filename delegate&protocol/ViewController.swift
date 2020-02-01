5912ac99eb483e0b7b7f4a3b1cb2bf8888c505e6
Fujio Nakatsuka <com.nifty.cbm01799>
2020-02-01 18:39:21 +0900

    Initial Commit

---
+++ delegate&protocol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  delegate&protocol
+//
+//  Created by 中塚富士雄 on 2020/02/01.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
